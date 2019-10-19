82612e42f4d98f1432ff31e639b2750d95955063
TakahashiAzuasa <56759367+TakahashiAzuasa@users.noreply.github.com>
2019-10-19 20:08:58 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '112',
+  ver: '110',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -42,9 +42,10 @@ jsproxy_config({
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        'ns.xuexi.icu': 1,
-        'ns.sub.xuexi.icu': 1,
-        'dns.xuexi.icu': 1,
+        'b.007.workers.dev': 1,
+        'b.hehe.workers.dev': 1,
+        'b.lulu.workers.dev': 1,
+        'b.jsproxy.workers.dev': 1,
       }
     }
   },
@@ -86,6 +87,9 @@ jsproxy_config({
   url_handler: {
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
+    },
+    'https://www.pornhub.com/': {
+      redir: 'https://php.net/'
     },
     'http://haha.com/': {
       content: 'Hello World'
